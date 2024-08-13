769fa3e207e2ddb0c20cf3fad14ef734fdca8656
tukumo-n9 <tukumon9.work@gmail.com>
2024-08-13 22:44:18 +0900

    スタイリングした

--- src/features/tickets/EditTicketForm.tsx
+++ src/features/tickets/EditTicketForm.tsx
@@ -94,102 +94,158 @@ export default function EditTicketForm() {
     <>
       {ticket ? (
         <>
-          <input
-            type="date"
-            value={date ?? ""}
-            placeholder="日付"
-            onChange={(e) => setDate(e.target.value)}
-          />
-          <select
-            value={racetrack}
-            onChange={(e) => setRacetrack(e.target.value)}
-          >
-            <option value="">競馬場</option>
-            <option value="京都">京都</option>
-            <option value="東京">東京</option>
-            <option value="新潟">新潟</option>
-          </select>
-          <select
-            value={raceNumber}
-            onChange={(e) => setRaceNumber(e.target.value)}
-          >
-            <option value="">レース</option>
-            <option value="1">1R</option>
-            <option value="2">2R</option>
-            <option value="3">3R</option>
-            <option value="4">4R</option>
-            <option value="5">5R</option>
-            <option value="6">6R</option>
-            <option value="7">7R</option>
-            <option value="8">8R</option>
-            <option value="9">9R</option>
-            <option value="10">10R</option>
-            <option value="11">11R</option>
-            <option value="12">12R</option>
-          </select>
-          <select
-            value={typeName ?? ""}
-            onChange={(e) => setTypeName(e.target.value)}
-          >
-            <option value="">券種</option>
-            <option value="単勝">単勝</option>
-            <option value="馬連">馬連</option>
-            <option value="三連単">三連単</option>
-          </select>
-          <label>
-            馬番
-            <select
-              value={typeNumbers.length === 0 ? [] : typeNumbers}
-              multiple={true}
-              onChange={(e) => {
-                const options = [...e.target.selectedOptions];
-                const values = options.map((option) => option.value);
-                setTypeNumbers(values);
+          <div className="bg-white p-4">
+            <div className="pb-4 border-b flex items-center">
+              <label htmlFor="date" className="w-1/3">
+                日付
+              </label>
+              <input
+                id="date"
+                type="date"
+                value={date ?? ""}
+                placeholder="日付"
+                className="p-1 border rounded grow"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+            <div className="pb-4 border-b mt-4 flex items-center">
+              <label htmlFor="racetrack" className="w-1/3">
+                競馬場
+              </label>
+              <select
+                id="racetrack"
+                value={racetrack}
+                className="p-1 border rounded grow"
+                onChange={(e) => setRacetrack(e.target.value)}
+              >
+                <option value="">---</option>
+                <option value="京都">京都</option>
+                <option value="東京">東京</option>
+                <option value="新潟">新潟</option>
+              </select>
+            </div>
+            <div className="pb-4 border-b mt-4 flex items-center">
+              <label htmlFor="raceNumber" className="w-1/3">
+                レース番号
+              </label>
+              <select
+                id="raceNumber"
+                value={raceNumber}
+                className="p-1 border rounded grow"
+                onChange={(e) => setRaceNumber(e.target.value)}
+              >
+                <option value="">---</option>
+                <option value="1">1R</option>
+                <option value="2">2R</option>
+                <option value="3">3R</option>
+                <option value="4">4R</option>
+                <option value="5">5R</option>
+                <option value="6">6R</option>
+                <option value="7">7R</option>
+                <option value="8">8R</option>
+                <option value="9">9R</option>
+                <option value="10">10R</option>
+                <option value="11">11R</option>
+                <option value="12">12R</option>
+              </select>
+            </div>
+            <div className="pb-4 border-b mt-4 flex items-center">
+              <label htmlFor="type" className="w-1/3">
+                券種
+              </label>
+              <select
+                id="type"
+                value={typeName ?? ""}
+                className="p-1 border rounded grow"
+                onChange={(e) => setTypeName(e.target.value)}
+              >
+                <option value="">---</option>
+                <option value="単勝">単勝</option>
+                <option value="馬連">馬連</option>
+                <option value="三連単">三連単</option>
+              </select>
+            </div>
+            <div className="pb-4 border-b mt-4 flex items-center">
+              <label htmlFor="number" className="w-1/3">
+                馬番
+              </label>
+              <select
+                id="number"
+                value={typeNumbers.length === 0 ? [] : typeNumbers}
+                multiple={true}
+                className="p-1 border rounded grow"
+                onChange={(e) => {
+                  const options = [...e.target.selectedOptions];
+                  const values = options.map((option) => option.value);
+                  setTypeNumbers(values);
+                }}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+              </select>
+            </div>
+            <div className="pb-4 border-b mt-4 flex items-center">
+              <label htmlFor="betAmount" className="w-1/3">
+                購入金額
+              </label>
+              <input
+                id="betAmount"
+                type="number"
+                value={betAmount ?? ""}
+                placeholder="購入金額"
+                step="100"
+                className="p-1 border rounded grow"
+                onChange={(e) => setBetAmount(e.target.value)}
+              />
+            </div>
+            <div className="mt-4 flex items-center">
+              <label htmlFor="payout" className="w-1/3">
+                払い戻し金額
+              </label>
+              <input
+                id="payout"
+                type="number"
+                value={payout ?? ""}
+                placeholder="払い戻し金額"
+                className="p-1 border rounded grow"
+                onChange={(e) => setPayout(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center gap-4 mt-8">
+            <button
+              type="button"
+              onClick={() => {
+                navigate(-1);
               }}
+              className="bg-transparent hover:bg-green-500 text-green-700 hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded"
             >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-            </select>
-          </label>
-          <input
-            type="number"
-            value={betAmount ?? ""}
-            placeholder="購入金額"
-            onChange={(e) => setBetAmount(e.target.value)}
-          />
-          <input
-            type="number"
-            value={payout ?? ""}
-            placeholder="払い戻し金額"
-            onChange={(e) => setPayout(e.target.value)}
-          />
-          <button type="button" onClick={onSaveTicketClicked}>
-            完了
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            戻る
-          </button>
+              戻る
+            </button>
+            <button
+              type="button"
+              onClick={onSaveTicketClicked}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
+              完了
+            </button>
+          </div>
         </>
       ) : (
         <p>馬券が見つかりませんでした。</p>
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -6,12 +6,19 @@ export default function Index() {
   const tickets = useAppSelector((state) => state.tickets);
   return (
     <>
-      <Link to={`/add-ticket/`}>馬券を追加</Link>
-      {Array.isArray(tickets) && tickets.length ? (
-        <TicketsList />
-      ) : (
-        <p>馬券が登録されていません。</p>
-      )}
+      <Link
+        to={`/add-ticket/`}
+        className="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        馬券を追加
+      </Link>
+      <div className="mt-4">
+        {Array.isArray(tickets) && tickets.length ? (
+          <TicketsList />
+        ) : (
+          <p>馬券が登録されていません。</p>
+        )}
+      </div>
     </>
   );
 }
