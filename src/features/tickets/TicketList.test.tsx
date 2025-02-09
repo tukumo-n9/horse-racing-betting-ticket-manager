f3b4460356cd34a7593a79b02e641ccfe03ac4e9
tukumo-n9 <tukumon9.work@gmail.com>
2025-02-09 23:13:26 +0900

    test: 馬券詳細コンポーネントのテストを追加した

---
+++ src/features/tickets/TicketList.test.tsx
@@ -0,0 +1,5 @@
+import { test, expect, describe } from "vitest";
+
+describe("馬券一覧コンポーネント", () => {
+  test("", () => {});
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { coverageConfigDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      exclude: ["./build/**", ...coverageConfigDefaults.exclude],
+    },
+  },
   build: {
     outDir: "build",
   },
